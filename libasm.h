ebadef754c5533fc269cad47582f1c3a13fe7d37
Brice Jacob <bjacob@student.42lyon.fr>
2020-12-17 08:35:52 +0100

    MAJ

--- libasm.h
+++ libasm.h
@@ -2,9 +2,17 @@
 # define LIBASM_H
 
 #include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-void	hello_world(void);
+#include <string.h>
+
 int		ft_strlen(char *str);
 char	*ft_strcpy(char * dst, const char * src);
+int     ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
+char	*ft_strdup(const char *s);
+
 
 #endif
